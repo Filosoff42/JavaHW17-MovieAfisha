6bb100063f23c5caced20b27366284ce65ac1f34
Grigoriy Kharitonskiy <argh94@list.ru>
2021-02-25 16:06:10 +0300

    Initial commit

---
+++ src/main/java/manager/MovieManager.java
@@ -0,0 +1,13 @@
+package manager;
+
+import domain.Movie;
+
+public class MovieManager {
+    private Movie[] movies;
+
+    public Movie[] getMoviesForFeed() {
+        // TODO: add logic
+        return null;
+    }
+}
+
